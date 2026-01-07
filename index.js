dec470cf7dd2bed4854d7cb7665f27302dfb031d
amberspake1 <amberspake1@gmail.com>
2026-01-07 11:50:41 -0800

    final tweaks made

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function toggleContrast() {
 
 function contact(event) {
     event.preventDefault()
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
+    const loading = document.querySelector(".modal__overlay--loading")
+    const success = document.querySelector(".modal__overlay--success")
     loading.classList += " modal__overlay--visible"
     emailjs
     .sendForm(
